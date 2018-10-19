51cac885e07c5c4ee86a8a4483e6e3b40be1faaf
terfno <sueda.takahito@gmail.com>
2018-10-19 22:33:22 +0900

    add water

---
+++ Water/Waterprocess.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waterprocess : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject water;
+    [SerializeField]
+    private GameObject waterFall;
+
+    private bool hasWater = false;
+
+    void Update()
+    {
+        if (transform.localEulerAngles.x > 80 && hasWater)
+        {
+            waterFall.SetActive(true);
+            hasWater = false;
+        }
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "waterTank")
+        {
+            water.SetActive(true);
+            hasWater = true;
+        }
+    }
+}
